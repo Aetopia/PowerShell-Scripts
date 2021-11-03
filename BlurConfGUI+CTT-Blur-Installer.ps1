1efccb9c7ada93ffa3311b785ace38d062b3b472
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-11-03 14:16:13 +0530

    Update and rename BlurGUI+CTT-Blur-Installer.ps1 to BlurConfGUI+CTT-Blur-Installer.ps1

--- BlurConfGUI+CTT-Blur-Installer.ps1
+++ BlurConfGUI+CTT-Blur-Installer.ps1
@@ -1,11 +1,11 @@
 # CTT Blur Installer made by Couleur.
-$Host.UI.RawUI.WindowTitle = "Blur GUI + CTT Blur Installer"
+$Host.UI.RawUI.WindowTitle = "BlurConfGUI + CTT Blur Installer"
 $ErrorActionPreference = 'SilentlyContinue'
 Import-Module BitsTransfer
 Write-Output "Installation Options
 
-1. Blur GUI + CTT Blur Installer
-2. Blur GUI Only (Replaces blurconf.bat with an AHK GUI.)
+1. BlurConfGUI + CTT Blur Installer
+2. BlurConfGUI Only (Replaces blurconf.bat with an AHK GUI.)
 
 "
 CHOICE /C 12 /N
@@ -18,7 +18,7 @@ Write-Output "Installing AutoHotkey..."
 Start-BitsTransfer -DisplayName "Downloading AutoHotkey" -Description " " "https://www.autohotkey.com/download/ahk-install.exe" "$env:TEMP\AutoHotkey.exe"
 Start-Process "$env:TEMP\AutoHotkey.exe" -Wait
 }
-Start-BitsTransfer -DisplayName 'Downloading "BlurGUI.ahk"...' -Description " " "https://raw.githubusercontent.com/couleur-tweak-tips/utils/main/Miscellaneous/BlurGUI.ahk" "$env:APPDATA\Microsoft\Windows\SendTo\BlurGUI.ahk"
+Start-BitsTransfer -DisplayName 'Downloading "BlurConfGUI.ahk"...' -Description " " "https://raw.githubusercontent.com/couleur-tweak-tips/utils/main/Miscellaneous/BlurConfGUI.ahk" "$env:APPDATA\Microsoft\Windows\SendTo\BlurConfGUI.ahk"
 cls
 # CTT's Blur Installer
 irm https://github.com/couleur-tweak-tips/utils/raw/main/Installers/blur.ps1 | iex
@@ -31,7 +31,7 @@ Write-Output "Installing AutoHotkey..."
 Start-BitsTransfer -DisplayName "Downloading AutoHotkey" -Description " " "https://www.autohotkey.com/download/ahk-install.exe" "$env:TEMP\AutoHotkey.exe"
 Start-Process "$env:TEMP\AutoHotkey.exe" -Wait
 }
-Start-BitsTransfer -DisplayName 'Downloading "BlurGUI.ahk"...' -Description " " "https://raw.githubusercontent.com/couleur-tweak-tips/utils/main/Miscellaneous/BlurGUI.ahk" "$env:APPDATA\Microsoft\Windows\SendTo\BlurGUI.ahk"
+Start-BitsTransfer -DisplayName 'Downloading "BlurGUI.ahk"...' -Description " " "https://raw.githubusercontent.com/couleur-tweak-tips/utils/main/Miscellaneous/BlurConfGUI.ahk" "$env:APPDATA\Microsoft\Windows\SendTo\BlurConfGUI.ahk"
 Write-Output '"BlurGUI.ahk" installed.'
 pause
 }
