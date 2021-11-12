9ab7063728d067b6fb99751ff2a98154d54f9eaf
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-11-12 10:35:43 +0530

    Added My First Module.

---
+++ PythonURL.psm1
@@ -0,0 +1,7 @@
+﻿function Start-PythonURL {
+param ($URL)
+    $PythonFile = (Invoke-RestMethod $URL)
+    $PythonFile | Set-Content "$env:TEMP\temp.py"
+    py.exe "$env:TEMP\temp.py"
+}
+Export-ModuleMember -Function Start-PythonURL
